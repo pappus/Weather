dda27e2ee54d3cb13609db50dd219300b507ad02
Rene Saroza <rene.saroza@publicissapient.com>
2021-03-09 16:02:48 -0500

    Initial commit of project files.

---
+++ weatherApp/weatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Rene Saroza on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
